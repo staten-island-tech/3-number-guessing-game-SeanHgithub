a59db3c54d4b2dbdf82db5f471eae3b704932591
SeanHgithub <seanhadley11@gmail.com>
2025-09-25 10:12:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import random
-a = 1
-b = 10
-print(random.randint(a,b))
+x = random.randint(1,10)
+print("Guess a number 1-10")
+guess = int(input("Enter a number"))
+while True:
+    if guess != x:
+        print("Guess again")
+    else:
+        break
 
 
